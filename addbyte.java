2339d7495203069be9fcbb01775ef973130a0843
Nishavennila <nishanaveen107@gmail.com>
2018-05-29 12:56:39 +0530

    basics

---
+++ addbyte.java
@@ -0,0 +1,11 @@
+public class addbyte
+{
+public static void main(String s[])
+{
+System.out.println("Welcome to java training!!!");
+byte one=-100;
+byte two=29;
+int three=one+two;
+System.out.println("three");
+}
+}
